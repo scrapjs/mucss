9567594ca47ddd3a5952870e0572ca56f4cab29d
Dmitry <df.creative@gmail.com>
2016-05-11 19:02:52 -0400

    Scope absent styles

--- prefix.js
+++ prefix.js
@@ -12,16 +12,18 @@ if (!styles) {
 	};
 }
 
-var pre = (Array.prototype.slice.call(styles)
-	.join('')
-	.match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
-)[1];
+else {
+	var pre = (Array.prototype.slice.call(styles)
+		.join('')
+		.match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
+	)[1];
 
-var dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1];
+	var dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1];
 
-module.exports = {
-	dom: dom,
-	lowercase: pre,
-	css: '-' + pre + '-',
-	js: pre[0].toUpperCase() + pre.substr(1)
-};
+	module.exports = {
+		dom: dom,
+		lowercase: pre,
+		css: '-' + pre + '-',
+		js: pre[0].toUpperCase() + pre.substr(1)
+	};
+}
